de8482b0c4219e467b6b5e4a9500671357b52aa5
Will Moritz <will@crowdfavorite.com>
2016-02-06 01:46:45 -0500

    loading any keyword, added many icons, front-end styles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var map;
 var service;
 var request;
 var mainAddress;
-var placeTypes = ['restaurant', 'hospital', 'cafe'];
+// Place types: https://developers.google.com/places/supported_types
+var placeTypes = ['restaurant', 'hospital', 'cafe', 'gym', 'cafe', 'atm', 'bar', 'subway_station'];
 
 function initMap() {
   mainAddress = new google.maps.LatLng(40.70876, -73.94139);
@@ -10,29 +11,52 @@ function initMap() {
   // Initialize main map
   map = new google.maps.Map(document.getElementById('map'), {
     center: mainAddress,
-    zoom: 15
+    zoom: 16
   });
 
+  var marker = new google.maps.Marker({
+    map: map,
+    position: mainAddress,
+    icon: "img/home.svg"
+  });
   // Request each place type
   for (var i = 0; i < placeTypes.length; i++) {
-    findPlaces(placeTypes[i]);
+    getPlaces(placeTypes[i]);
   }
-
 
 }
 
-var findPlaces = function(place) {
+// Gets place data from Google
+var getPlaces = function(type) {
   request = {
     location: mainAddress,
-    radius: '500',
-    types: [place]
+    radius: '700',
+    types: [type]
   }
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, function(results) {
-    console.log("Request for " + place);
     for (var j = 0; j < results.length; j++) {
-      console.log(place, '|', results[j].name);
+      createMarker(results[j], type);
     }
+  });
+}
+
+// Places markers on map
+var createMarker = function(place, type) {
+  var placeLoc = place.geometry.location;
+  var image = {
+    url: 'img/' + type + '.png',
+    size: new google.maps.Size(29, 29)
+  }
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: image
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
